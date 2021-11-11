4fc313b02722ac1e23d3c0d1a29b07d4e29374bf
nikhilcredex <94034479+nikhilcredex@users.noreply.github.com>
2021-11-11 20:09:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const dbConfig = require('./config/config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Signup Api"});
+});
+
+require('./app/routes/routes.js')(app);
+
+// listen for requests
+app.listen(7000, () => {
+    console.log("Server is listening on port 7000");
+});
